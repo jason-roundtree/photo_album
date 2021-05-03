2e114ee6e017a760a98fe274aca43be3d6fb14a0
JASON ROUNDTREE <jasontree123@gmail.com>
2021-05-02 20:02:58 -0700

    Change api endpoint names

--- pages/api/album/[album-collection]/[album-name].js
+++ pages/api/album/[album-collection]/[album-name].js
@@ -1,14 +1,15 @@
 const cloudinary = require('cloudinary').v2
 
 export default (req, res) => {
-    // console.log('req', req)
+    console.log('req', req)
     cloudinary
         .search
-        .expression(`${req.query.collection}/${req.query.name}/*`)
+        .expression(`${req.query.album-collection}/${req.query.album-name}/*`)
         .with_field('context')
         .execute()
         .then(result => {
             // console.log('folder path result: ', result)
             res.json(result) 
         })
+        .catch(err => console.log('error: ', err))
 }
