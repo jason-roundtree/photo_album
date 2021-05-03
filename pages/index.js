60c49d56de03a57f61d8e4a87304f3528ff8e494
JASON ROUNDTREE <jasontree123@gmail.com>
2021-05-03 08:14:43 -0700

    Add cors middleware

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,47 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 
-export default function Home() {
+async function getPhotoAlbum(folderPath) {
+  console.log(`https://photo-album-six.vercel.app/api/album/${folderPath}`)
+  try {
+    const res = await fetch(`https://photo-album-six.vercel.app/api/album/${folderPath}`)
+    if (res.ok) {
+      // console.log('getPhotoAlbum res: ', res)
+      return res.json()
+    } else {
+      throw new Error(res)
+    }
+  } catch(err) {
+    console.log(`error fetching ${folderPath} folder: `, err)
+  }
+}
+
+export default function Home({ albumPaths }) {
+  const [photoAlbumPaths, setPhotoAlbumPaths] = useState(albumPaths)
+  const [photoAlbums, setPhotoAlbums] = useState([])
+  console.log(`photoAlbumPaths: `, photoAlbumPaths)
+  console.log(`photoAlbums: `, photoAlbums)
+
+  useEffect(() => {
+    const allPhotoAlbumPromises = photoAlbumPaths.map(({ path }) => {
+      getPhotoAlbum(path)
+    })
+
+    Promise.all(allPhotoAlbumPromises)
+      .then(allPhotoAlbumData => {
+        setPhotoAlbums(allPhotoAlbumData)
+      })
+      .catch(err => console.log('error resolving allPhotoAlbumPromises: ', err))
+  }, [photoAlbumPaths])
+
   return (
     <div>
       <Head>
         <title>Photo Albums</title>
-        <meta name="description" content="Albums of photography by Jason Roundtree" />
+        <meta name="description" content="Photography by Jason Roundtree" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -30,14 +63,20 @@ export default function Home() {
   )
 }
 
-// export async function getStaticProps() {
-//   const res = await fetch('https://photo-album-six.vercel.app/api/album-paths')
+// export async function getStaticPaths() {
+//   const res = await fetch('https://photo-album-six.vercel.app/api/albumPaths')
 //   console.log(`res: `, res)
-//   // const albumPaths = await res.json()
-//   // console.log(`albumPaths: `, albumPaths)
-//   return {
-//     props: {
-//       a: 'hello'
-//     },
-//   }
+//   const albumPaths = await res.json()
+//   const paths
 // }
+
+export async function getStaticProps() {
+  const res = await fetch('https://photo-album-six.vercel.app/api/paths')
+  // const a = await res.text()
+  // console.log(`res.json(): `, a)
+  const albumPaths = await res.json()
+  console.log(`albumPaths: `, albumPaths)
+  return {
+    props: { albumPaths: albumPaths.folders }
+  }
+}
